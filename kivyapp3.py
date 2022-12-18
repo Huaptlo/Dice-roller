b64d9740855fcb0fd56d1bb214133b65f857ee86
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2022-12-18 12:06:57 +0200

    Added output box

--- kivyapp3.py
+++ kivyapp3.py
@@ -2,6 +2,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
 from kivy.config import Config
 from kivy.uix.dropdown import DropDown
 from kivy.base import runTouchApp
@@ -25,18 +26,22 @@ class MyApp(App):
         dropdown.bind(on_select = lambda instance, x: setattr(mainbutton, 'text', x))
         
         btn2 = Button(text ='Amount', size_hint =(.3, .3),
-                     background_color =(.3, .6, .7, 1),
+                    background_color =(.3, .6, .7, 1),
                     pos_hint ={'x':.6, 'y':.6 })
 
         btn3 = Button(text ='Roll', size_hint =(.3, .2),
-                     background_color =(.3, .6, .7, 1),
+                    background_color =(.3, .6, .7, 1),
                     pos_hint ={'x':.35, 'y':.35 })
 
-        # result_box = 
+        result_box = TextInput(text = "0",
+                    pos_hint = {'x':.1, 'y':.1},
+                    font_size = 45,
+                    size_hint = (.8,.15))
 
         Fl.add_widget(mainbutton) 
         Fl.add_widget(btn2)
         Fl.add_widget(btn3)
+        Fl.add_widget(result_box)
 
         return Fl
 
