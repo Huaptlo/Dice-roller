a0cf5286962aa1aba16e09448717c5ff829d095f
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2023-03-07 11:07:08 +0200

    name changes

--- main.py
+++ main.py
@@ -1,107 +1,58 @@
 import kivy
-from kivy.app import App
-from kivy.core.window import Window
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
-from kivy.config import Config
-from kivy.uix.dropdown import DropDown
-from kivy.base import runTouchApp
 kivy.require('2.1.0')
-Config.set('graphics', 'resizable', True)
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.slider import Slider
+import dice
 
-class DiceRoller(App):
-  
-    def build(self):
-        Fl = FloatLayout()
+class DiceRoller(FloatLayout):
+    def __init__(self, **kwargs):
+        super(DiceRoller, self).__init__(**kwargs)
+
+        # Dropdown to choose dice type
         dropdown = DropDown()
+        for sides in [4, 6, 8, 10, 12, 20, 100]:
+            btn = Button(text=str(sides), size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            dropdown.add_widget(btn)
+        self.dice_type = Button(text='4', size_hint=(0.3, 0.1), pos_hint={'x': 0.05, 'y': 0.8})
+        self.dice_type.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=lambda instance, x: setattr(self.dice_type, 'text', x))
+        self.add_widget(self.dice_type)
 
-        for index in range(7):
-            btn1 = Button(text ='D4', size_hint_y = None, height = 30,
-                    background_color =(.3, .6, .7, 1))
-            btn1.bind(on_release = lambda btn: dropdown.select(btn.text))
+        # Scroll bar to choose number of dice to roll
+        self.num_dice_label = Label(text='Number of Dice: 1', size_hint=(0.3, 0.1), pos_hint={'x': 0.05, 'y': 0.65})
+        self.add_widget(self.num_dice_label)
+        self.num_dice_slider = Slider(min=1, max=20, value=1, step=1, size_hint=(0.4, 0.1), pos_hint={'x': 0.35, 'y': 0.65})
+        self.num_dice_slider.bind(value=self.update_num_dice)
+        self.add_widget(self.num_dice_slider)
 
-            btn2 = Button(text ='D6', size_hint_y = None, height = 30,
-                    background_color =(.3, .6, .7, 1))
-            btn2.bind(on_release = lambda btn: dropdown.select(btn.text))
+        # Button to roll dice
+        self.roll_button = Button(text='Roll', size_hint=(0.3, 0.1), pos_hint={'x': 0.05, 'y': 0.5})
+        self.roll_button.bind(on_release=self.roll_dice)
+        self.add_widget(self.roll_button)
 
-            btn3 = Button(text ='D8', size_hint_y = None, height = 30,
-                    background_color =(.3, .6, .7, 1))
-            btn3.bind(on_release = lambda btn: dropdown.select(btn.text))
+        # Field to display dice result
+        self.result_label = Label(text='Result: ', size_hint=(0.9, 0.1), pos_hint={'x': 0.05, 'y': 0.3})
+        self.add_widget(self.result_label)
 
-            btn4 = Button(text ='D10', size_hint_y = None, height = 30,
-                    background_color =(.3, .6, .7, 1))
-            btn4.bind(on_release = lambda btn: dropdown.select(btn.text))
+    def update_num_dice(self, instance, value):
+        self.num_dice_label.text = 'Number of Dice: ' + str(int(value))
 
-            btn5 = Button(text ='D12', size_hint_y = None, height = 30,
-                    background_color =(.3, .6, .7, 1))
-            btn5.bind(on_release = lambda btn: dropdown.select(btn.text))
+    def roll_dice(self, instance):
+        num_dice = int(self.num_dice_slider.value)
+        dice_type = int(self.dice_type.text)
+        result = str(f'{num_dice}d{dice_type}')
+        self.result_label.text = str(dice.roll(result))
 
-            btn6 = Button(text ='D20', size_hint_y = None, height = 30,
-                    background_color =(.3, .6, .7, 1))
-            btn6.bind(on_release = lambda btn: dropdown.select(btn.text))
+class DiceRollerApp(App):
+    def build(self):
+        return DiceRoller()
 
-            btn7 = Button(text ='D100', size_hint_y = None, height = 30,
-                    background_color =(.3, .6, .7, 1))
-            btn7.bind(on_release = lambda btn: dropdown.select(btn.text))
-
-            dropdown.add_widget(btn1)
-            dropdown.add_widget(btn2)
-            dropdown.add_widget(btn3)
-            dropdown.add_widget(btn4)
-            dropdown.add_widget(btn5)
-            dropdown.add_widget(btn6)
-            dropdown.add_widget(btn7)
-
-            break
-
-        main_btn = Button(text ='Dice', size_hint =(.3, .2),
-                            background_color =(.3, .6, .7, 1), pos_hint ={'x':.1, 'y':.5})
-        main_btn.bind(on_release = dropdown.open)
-        dropdown.bind(on_select = lambda instance, x: setattr(main_btn, 'text', x))
-
-        for i in range(1,11):
-            btn8 = Button(text ='%d' % i, size_hint_y = None, height = 30,
-                    background_color =(.3, .6, .7, 1))
-            btn8.bind(on_release = lambda btn: dropdown.select(btn.text))
-
-            dropdown.add_widget(btn8)
-
-        dice_amount_btn = Button(text ='Amount', size_hint =(.3, .2),
-                            background_color =(.3, .6, .7, 1), pos_hint ={'x':.6, 'y':.5})
-        dice_amount_btn.bind(on_release = dropdown.open)
-        dropdown.bind(on_select = lambda instance, x: setattr(dice_amount_btn, 'text', x))
-        
-        # dice_amount_btn = Button(text ='Amount', size_hint =(.3, .2),
-        #             background_color =(.3, .6, .7, 1),
-        #             pos_hint ={'x':.6, 'y':.5 })
-
-        roll_btn = Button(text ='Roll', size_hint =(.5, .2),
-                    background_color =(.3, .6, .7, 1),
-                    pos_hint ={'x':.25, 'y':.2 })
-
-        result_box = TextInput(text = "0",
-                    pos_hint = {'x':.1, 'y':.75},
-                    font_size = 45,
-                    size_hint = (.8,.15))
-
-        self.close_btn = Button(text = 'X', size_hint =(.05, .05),
-                    background_color =(.3, .6, .7, 1),
-                    pos_hint ={'x':.9, 'y':.9})
-        self.close_btn.bind(on_release=self.close_app)
-
-        Fl.add_widget(main_btn) 
-        Fl.add_widget(dice_amount_btn)
-        Fl.add_widget(roll_btn)
-        Fl.add_widget(result_box)
-        Fl.add_widget(self.close_btn)
-
-        return Fl
-
-    def close_app(self, instance):
-        App.get_running_app().stop()
-
-# Window.fullscreen = 'auto'
-
-if __name__ == "__main__":
-    DiceRoller().run()
+if __name__ == '__main__':
+    DiceRollerApp().run()
